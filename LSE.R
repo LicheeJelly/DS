470da00a5ae7ccf62fe5786be960792c2f44a0dd
LicheeJelly <jerryli@Jerrys-MacBook-Pro.local>
2020-04-27 22:45:51 -0400

    Least Square Error

---
+++ LSE.R
@@ -0,0 +1,23 @@
+library(HistData)
+library(tidyverse)
+library(dslabs)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>% 
+    filter(gender == "male") %>% 
+    group_by(family) %>%
+    sample_n(1) %>%
+    ungroup() %>%
+    select(father, childHeight) %>%
+    rename(son = childHeight)
+  rss <- function(beta0, beta1, data){
+    resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+    return(sum(resid^2))
+  }
+
+# plot RSS as a function of beta1 when beta0=25
+  beta1 = seq(0, 1, len=nrow(galton_heights))
+  results <- data.frame(beta1 = beta1,
+                        rss = sapply(beta1, rss, beta0 = 25))
+  results %>% ggplot(aes(beta1, rss)) +  
+    geom_line(aes(beta1, rss))
